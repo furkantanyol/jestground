05124b30cf9f343b5ef7bae70660944cebba6d83
furkantanyol <furkantanyol@gmail.com>
2019-12-13 14:06:33 +1100

    added more tests

---
+++ tests/Fromise.js
@@ -0,0 +1,82 @@
+const PENDING = 'PENDING';
+const RESOLVED = 'RESOLVED';
+const REJECTED = 'REJECTED';
+
+export default class Fromise {
+  constructor(executor) {
+    this.state = PENDING;
+    this.value = null;
+    this.queue = [];
+    try {
+      executor(this.resolve, this.reject);
+    } catch (err) {
+      this.reject(err);
+    }
+  }
+
+  resolve = value => {
+    if (this.state !== PENDING) return;
+    this.state = RESOLVED;
+    this.value = value;
+
+    try {
+      this.queue.forEach(({ onResolved, onRejected }) => {
+        if (this.state === RESOLVED) {
+          onResolved(this.value);
+        }
+        if (this.state === REJECTED) {
+          onRejected(this.value);
+        }
+      });
+    } catch (error) {
+      this.queue = [];
+      this.reject(error);
+    }
+  };
+
+  reject = error => {
+    if (this.state !== PENDING) return;
+
+    this.state = REJECTED;
+    this.value = error;
+
+    try {
+      this.queue.forEach(({ onResolved, onRejected }) => {
+        if (this.state === RESOLVED) {
+          onResolved(this.value);
+        }
+        if (this.state === REJECTED) {
+          onRejected(this.value);
+        }
+      });
+    } catch (error) {
+      this.queue = [];
+      this.reject(error);
+    }
+  };
+
+  then = (onResolved, onRejected) => {
+    let newPromise = new Fromise(() => {});
+
+    if (this.state === RESOLVED) {
+      try {
+        this.value = onResolved(this.value);
+        newPromise.resolve(this.value);
+      } catch (err) {
+        newPromise.reject(err);
+      }
+    }
+    if (this.state === REJECTED) {
+      try {
+        this.value = onRejected(this.value);
+        newPromise.resolve(this.value);
+      } catch (err) {
+        newPromise.reject(err);
+      }
+    }
+    if (this.state === PENDING) {
+      this.queue.push({ onResolved, onRejected, newPromise });
+    }
+    return newPromise;
+  };
+}
--- tests/fromise.test.js
+++ tests/fromise.test.js
@@ -1,65 +1,7 @@
-import chalk from 'chalk';
-
 const PENDING = 'PENDING';
 const RESOLVED = 'RESOLVED';
 const REJECTED = 'REJECTED';
-
-class Fromise {
-  constructor(executor) {
-    this.state = PENDING;
-    this.value = null;
-    this.queue = [];
-    try {
-      executor(this.resolve, this.reject);
-    } catch (err) {
-      this.reject(err);
-    }
-  }
-
-  resolve = value => {
-    if (this.state !== PENDING) return;
-    this.state = RESOLVED;
-    this.value = value;
-
-    try {
-      this.queue.forEach(({ onResolved, onRejected }) => {
-        if (this.state === RESOLVED) {
-          this.value = onResolved(this.value);
-        }
-        if (this.state === REJECTED) {
-          this.value = onRejected(this.value);
-        }
-      });
-    } catch (error) {
-      this.queue = [];
-      this.reject(error);
-    }
-  };
-
-  reject = error => {
-    if (this.state !== PENDING) return;
-
-    this.state = REJECTED;
-    this.value = error;
-
-    this.queue.forEach(({ onResolved, onRejected }) => {
-      this.then(onResolved, onRejected);
-    });
-  };
-
-  then = (onResolved, onRejected) => {
-    if (this.state === RESOLVED) {
-      onResolved(this.value);
-    }
-    if (this.state === REJECTED) {
-      onRejected(this.value);
-    }
-    if (this.state === PENDING) {
-      this.queue.push({ onResolved, onRejected });
-    }
-    return new Fromise();
-  };
-}
+import Fromise from './Fromise';
 
 describe('Fromise Tests Init', () => {
   it('receives an executor function when constructed which is called immediately', () => {
@@ -181,7 +123,7 @@ describe('Fromise Tests Init', () => {
     const onResolved = jest.fn();
 
     const promise = new Fromise((res, rej) => {
-      setTimeout(() => res(value), 1000);
+      setTimeout(() => res(value), 10);
     });
 
     promise.then(onResolved);
@@ -189,26 +131,25 @@ describe('Fromise Tests Init', () => {
     setTimeout(() => {
       // should have been called twice
       expect(onResolved.mock.calls.length).toBe(1);
-      console.log('öeyeyafsadf', onResolved.mock.calls);
-      expect(onResolved.mock.calls[1][0]).toBe(value);
-      done();
-    }, 2000);
+      expect(onResolved.mock.calls[0][0]).toBe(value);
+      promise.then(onResolved);
+    }, 20);
 
     // should not be called immediately
     expect(onResolved.mock.calls.length).toBe(0);
 
-    setTimeout(function() {
+    setTimeout(() => {
       // should have been called twice
       expect(onResolved.mock.calls.length).toBe(2);
       expect(onResolved.mock.calls[1][0]).toBe(value);
       done();
-    }, 4000);
+    }, 40);
   });
 
   it('should queue callbacks when the promise is not rejected immediately', done => {
     const error = 'Fuck!';
     const promise = new Fromise((res, rej) => {
-      setTimeout(() => rej(error), 1);
+      setTimeout(() => rej(error), 10);
     });
 
     const onRejected = jest.fn();
@@ -218,11 +159,9 @@ describe('Fromise Tests Init', () => {
     setTimeout(() => {
       // should have been called once
       expect(onRejected.mock.calls.length).toBe(1);
-      expect(onRejected.mock.calls[0][0]).toBe(reason);
+      expect(onRejected.mock.calls[0][0]).toBe(error);
       promise.then(null, onRejected);
-
-      done();
-    }, 5);
+    }, 20);
 
     // should not be called immediately
     expect(onRejected.mock.calls.length).toBe(0);
@@ -230,7 +169,90 @@ describe('Fromise Tests Init', () => {
     setTimeout(() => {
       // should have been called twice
       expect(onRejected.mock.calls.length).toBe(2);
-      expect(onRejected.mock.calls[1][0]).toBe(reason);
+      expect(onRejected.mock.calls[1][0]).toBe(error);
+      done();
+    }, 40);
+  });
+
+  it('.then should return a new promise', () => {
+    expect(() => {
+      const qOnResolved = jest.fn();
+      const rOnResolved = jest.fn();
+      const p = new Fromise(res => res());
+      const q = p.then(qOnResolved);
+      const r = q.then(rOnResolved);
+    }).not.toThrow();
+  });
+
+  it("if .then's onResolved is called without errors it should transition to RESOLVED", () => {
+    const value = ':)';
+    const secondThenCb = jest.fn();
+    const promise = new Fromise(res => res())
+      .then(() => value)
+      .then(secondThenCb);
+    expect(secondThenCb.mock.calls.length).toBe(1);
+    expect(secondThenCb.mock.calls[0][0]).toBe(value);
+  });
+
+  it("if .then's onRejected is called without errors it should transition to Resolved", () => {
+    const error = ':)';
+    const secondThenCb = jest.fn();
+    new Fromise((_, rej) => rej()).then(null, () => error).then(secondThenCb);
+    expect(secondThenCb.mock.calls.length).toBe(1);
+    expect(secondThenCb.mock.calls[0][0]).toBe(error);
+  });
+
+  it("if .then's onResolved is called and has an error it should transition to REJECTED", () => {
+    const reason = new Error('I failed :(');
+    const secondThenCb = jest.fn();
+    new Fromise(res => res())
+      .then(() => {
+        throw reason;
+      })
+      .then(null, secondThenCb);
+    expect(secondThenCb.mock.calls.length).toBe(1);
+    expect(secondThenCb.mock.calls[0][0]).toBe(reason);
+  });
+
+  it("if .then's onRejected is called and has an error it should transition to REJECTED", () => {
+    const reason = new Error('I failed :(');
+    const secondThenCb = jest.fn();
+    new Fromise((res, rej) => rej())
+      .then(null, () => {
+        throw reason;
+      })
+      .then(null, secondThenCb);
+    expect(secondThenCb.mock.calls.length).toBe(1);
+    expect(secondThenCb.mock.calls[0][0]).toBe(reason);
+  });
+
+  it('if a handler returns a promise, the previous promise should adopt the state of the returned promise', () => {
+    const value = 23;
+    const secondThenCb = jest.fn();
+    new Fromise((res, rej) => setTimeout(() => res(), 0))
+      .then(() => {
+        'promiz dönecem';
+        return new Fromise((res, rej) => {
+          setTimeout(() => res(value), 0);
+        });
+      })
+      .then(val => {
+        secondThenCb();
+      });
+
+    expect(secondThenCb.mock.calls.length).toBe(1);
+    expect(secondThenCb.mock.calls[0][0]).toBe(value);
+  });
+
+  it('if a handler returns a promise resolved in the future, the previous promise should adopt its value', done => {
+    const value = ':)';
+    const secondThenCb = jest.fn();
+    new Fromise(res => setTimeout(res, 0))
+      .then(() => new Fromise(resolve => setTimeout(resolve, 0, value)))
+      .then(secondThenCb);
+    setTimeout(() => {
+      expect(secondThenCb.mock.calls.length).toBe(1);
+      expect(secondThenCb.mock.calls[0][0]).toBe(value);
       done();
     }, 10);
   });
