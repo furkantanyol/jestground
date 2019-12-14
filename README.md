969ca276ebcad916277799fdcfa9eefeb96fdc9b
furkantanyol <furkantanyol@gmail.com>
2019-12-14 11:21:38 +1100

    collision detection && eslint

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # jestground
 
 Just a JS test playground using Jest
+
+- vDOM
+- own Promises(I have some questions about this)
+- Collision Detection
+- Implement drag and drop from scratch
+- Implement JSON parser from scratch
--- src/mySquare/index.js
+++ src/mySquare/index.js
@@ -1,110 +1,156 @@
-const app = document.getElementById('app');
-const mySquare = document.createElement('div');
-
 const keys = {
   left: 'ArrowLeft',
   up: 'ArrowUp',
   right: 'ArrowRight',
-  down: 'ArrowDown'
-};
+  down: 'ArrowDown',
+}
 
 function translate(x, y) {
-  return `translate(${x}px,${y}px)`;
+  return `translate(${x}px,${y}px)`
+}
+
+function detectCollision(collider, victim) {
+  const colliderRect = collider.getBoundingClientRect()
+  const victimRect = victim.getBoundingClientRect()
+  const {
+    width: colliderRectWidth,
+    height: colliderRectHeight,
+    left: colliderRectLeft,
+    right: colliderRectRight,
+    top: colliderRectTop,
+    bottom: colliderRectBottom,
+  } = colliderRect
+
+  const {
+    left: victimRectLeft,
+    right: victimRectRight,
+    top: victimRectTop,
+    bottom: victimRectBottom,
+  } = victimRect
+
+  const collisionLeft = colliderRectLeft + colliderRectWidth > victimRectLeft
+  const collisionRight = colliderRectRight - colliderRectWidth < victimRectRight
+  const collisionTop = colliderRectTop + colliderRectHeight > victimRectTop
+  const collisionBottom =
+    colliderRectBottom - colliderRectHeight < victimRectBottom
+
+  if (collisionLeft && collisionRight && collisionTop && collisionBottom) {
+    // eslint-disable-next-line no-param-reassign
+    victim.style.opacity = 0
+  }
 }
 
 const onKeydown = e => {
-  const mySquare = document.getElementById('my-square');
-  const { translateX, translateY } = mySquare;
-  let newTranslateX, newTranslateY;
+  const mySquare = document.getElementById('my-square')
+  const otherSquare = document.getElementById('other-square')
+
+  const { translateX, translateY } = mySquare
+  let newTranslateX
+  let newTranslateY
   switch (e.key) {
     case keys.left:
-      newTranslateX = translateX - 10;
-      mySquare.translateX = newTranslateX;
-      mySquare.style.transform = translate(newTranslateX, translateY);
-      break;
+      newTranslateX = translateX - 10
+      mySquare.translateX = newTranslateX
+      mySquare.style.transform = translate(newTranslateX, translateY)
+      break
 
     case keys.right:
-      newTranslateX = translateX + 10;
-      mySquare.translateX = newTranslateX;
-      mySquare.style.transform = translate(newTranslateX, translateY);
-      break;
+      newTranslateX = translateX + 10
+      mySquare.translateX = newTranslateX
+      mySquare.style.transform = translate(newTranslateX, translateY)
+      break
 
     case keys.up:
-      newTranslateY = translateY - 10;
-      mySquare.translateY = newTranslateY;
-      mySquare.style.transform = translate(translateX, newTranslateY);
-      break;
+      newTranslateY = translateY - 10
+      mySquare.translateY = newTranslateY
+      mySquare.style.transform = translate(translateX, newTranslateY)
+      break
 
     case keys.down:
-      newTranslateY = translateY + 10;
-      mySquare.translateY = newTranslateY;
-      mySquare.style.transform = translate(translateX, newTranslateY);
-      break;
+      newTranslateY = translateY + 10
+      mySquare.translateY = newTranslateY
+      mySquare.style.transform = translate(translateX, newTranslateY)
+      break
 
     default:
-      break;
+      break
   }
-};
+
+  detectCollision(mySquare, otherSquare)
+}
 
 const throttle = (func, limit) => {
-  let inThrottle;
-  return function() {
-    const args = arguments;
-    const context = this;
+  let inThrottle
+  return () => {
+    // eslint-disable-next-line no-undef
+    const args = arguments
+    const context = this
     if (!inThrottle) {
-      func.apply(context, args);
-      inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
+      func.apply(context, args)
+      inThrottle = true
+      setTimeout(() => {
+        inThrottle = false
+      }, limit)
     }
-  };
-};
+  }
+}
 
-const throttledMouseMove = throttle(e => onMousemove(e), 100);
-
-const onMousedown = e => {
-  document.addEventListener('mousemove', throttledMouseMove);
-};
+const onMousedown = () => {
+  document.addEventListener('mousemove', throttledMouseMove)
+}
 
 const onMousemove = e => {
-  const mySquare = document.getElementById('my-square');
+  const mySquare = document.getElementById('my-square')
+  const otherSquare = document.getElementById('other-square')
 
-  mySquare.removeEventListener('mousedown', onMousedown);
+  mySquare.removeEventListener('mousedown', onMousedown)
 
-  const { x, y } = e;
+  const { x, y } = e
 
   // TODO: find out how to get these constants in a better way
-  const deltaX = x - 517;
-  const deltaY = y - 391;
+  const deltaX = x - 517
+  const deltaY = y - 391
 
-  mySquare.translateX = deltaX;
-  mySquare.translateY = deltaY;
-  mySquare.style.transform = translate(
-    mySquare.translateX,
-    mySquare.translateY
-  );
-};
+  mySquare.translateX = deltaX
+  mySquare.translateY = deltaY
+  mySquare.style.transform = translate(mySquare.translateX, mySquare.translateY)
 
-const onMouseup = e => {
-  const mySquare = document.getElementById('my-square');
+  detectCollision(mySquare, otherSquare)
+}
+const throttledMouseMove = throttle(e => onMousemove(e), 100)
 
-  document.removeEventListener('mousemove', throttledMouseMove);
-  mySquare.addEventListener('mousedown', onMousedown);
-};
+const onMouseup = () => {
+  const mySquare = document.getElementById('my-square')
 
-mySquare.translateX = 0;
-mySquare.translateY = 0;
-mySquare.moving = false;
-mySquare.setAttribute('id', 'my-square');
+  document.removeEventListener('mousemove', throttledMouseMove)
+  mySquare.addEventListener('mousedown', onMousedown)
+}
+
+const app = document.getElementById('app')
+const mySquare = document.createElement('div')
+const otherSquare = document.createElement('div')
+
+mySquare.translateX = 0
+mySquare.translateY = 0
+mySquare.moving = false
+mySquare.setAttribute('id', 'my-square')
 mySquare.setAttribute(
   'style',
   `width: 20px; height: 20px; background-color: red; position: absolute; left: 50%; top:50%; transition: transform 0.095s ease-in-out; transform: ${translate(
     mySquare.translateX,
-    mySquare.translateY
-  )};`
-);
+    mySquare.translateY,
+  )};`,
+)
 
-document.addEventListener('keydown', onKeydown);
-mySquare.addEventListener('mousedown', onMousedown);
-document.addEventListener('mouseup', onMouseup);
+otherSquare.setAttribute('id', 'other-square')
+otherSquare.setAttribute(
+  'style',
+  'width: 50px; height: 50px; background-color: blue; position: absolute; left: 25%; top: 25%; transition: opacity 1s ease-in-out;',
+)
 
-app.appendChild(mySquare);
+document.addEventListener('keydown', onKeydown)
+mySquare.addEventListener('mousedown', onMousedown)
+document.addEventListener('mouseup', onMouseup)
+
+app.appendChild(otherSquare)
+app.appendChild(mySquare)
